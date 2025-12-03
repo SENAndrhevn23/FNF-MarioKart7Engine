32b759adfa873d0316c04fa5f84636e119b0d1ac
Andre Nicholas Jr <pumpgaming2@gmail.com>
2025-12-03 16:24:43 -0600

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,250 +1,101 @@
-package;
+name: Build
 
-#if android
-import android.content.Context;
-#end
+on:
+  push:
+    branches: [ main, experimental ]
+  pull_request:
+    branches: [ main, experimental ]
+  workflow_dispatch:
 
-import debug.FPSCounter;
+jobs:
+  buildLinux:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4.1.7
 
-import flixel.FlxGame;
-import flixel.FlxState;
-import haxe.io.Path;
-import openfl.Lib;
-import openfl.display.Sprite;
-import openfl.display.StageScaleMode;
-import lime.app.Application;
-import states.TitleState;
+      - uses: krdlab/setup-haxe@master
+        with:
+          haxe-version: 4.3.4
 
-#if HSCRIPT_ALLOWED
-import crowplexus.iris.Iris;
-import psychlua.HScript.HScriptInfos;
-#end
+      - name: Install Haxelib and Dependencies
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y libvlc-dev libvlccore-dev
+          haxelib setup ~/haxelib
+          haxelib install hxcpp > /dev/null --quiet
+          haxelib install hxvlc > /dev/null --quiet
+          chmod +x ./setup/unix.sh
+          sh ./setup/unix.sh
 
-#if (linux || mac)
-import lime.graphics.Image;
-#end
+      - name: Create Version Tag
+        run: echo "${{ github.run_id }}" > VERSION
 
-#if desktop
-import backend.ALSoftConfig;
-#end
+      - name: Compile
+        run: haxelib run lime build linux --app-version="4.0.0-${{ github.run_id }}" -D officialBuild -D VIDEOS_ALLOWED
 
-#if CRASH_HANDLER
-import openfl.events.UncaughtErrorEvent;
-import haxe.CallStack;
-import haxe.io.Path;
-#end
+      - name: Publish Artifact
+        uses: actions/upload-artifact@v4.3.4
+        with:
+          name: linuxBuild
+          path: 'export/release/linux/bin'
 
-import backend.Highscore;
+  buildWindows:
+    runs-on: windows-latest
+    steps:
+      - uses: actions/checkout@v4.1.7
 
-#if VIDEOS_ALLOWED
-import hxvlc.VLC;
-#end
+      - uses: krdlab/setup-haxe@master
+        with:
+          haxe-version: 4.3.4
 
-class Main extends Sprite
-{
-	public static final game = {
-		width: 1280,
-		height: 720,
-		initialState: TitleState,
-		framerate: 60,
-		skipSplash: true,
-		startFullscreen: false
-	};
+      - name: Install Haxelib and Dependencies
+        run: |
+          haxelib setup C:/haxelib
+          haxelib install hxcpp > nul
+          haxelib install hxvlc > nul
+          .\setup\windows.bat
+        shell: cmd
 
-	public static var fpsVar:FPSCounter;
+      - name: Create Version Tag
+        run: echo "${{ github.run_id }}" > VERSION
+        shell: cmd
 
-	public static function main():Void
-	{
-		Lib.current.addChild(new Main());
-	}
+      - name: Compile
+        run: haxelib run lime build windows --64bits --app-version="4.0.0-${{ github.run_id }}" -D officialBuild
+        shell: cmd
 
-	public function new()
-	{
-		super();
+      - name: Publish Artifact
+        uses: actions/upload-artifact@v4.3.4
+        with:
+          name: windowsBuild
+          path: export/release/windows/bin
 
-		#if (cpp && windows)
-		backend.Native.fixScaling();
-		#end
+  buildMac:
+    runs-on: macos-15
+    steps:
+      - uses: actions/checkout@v4.1.7
 
-		#if android
-		Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
-		#elseif ios
-		Sys.setCwd(lime.system.System.applicationStorageDirectory);
-		#end
+      - uses: krdlab/setup-haxe@master
+        with:
+          haxe-version: 4.3.4
 
-		#if VIDEOS_ALLOWED
-		playStartupVideo();
-		#else
-		startEngine();
-		#end
-	}
+      - name: Install Haxelib and Dependencies
+        run: |
+          brew install vlc
+          haxelib setup ~/haxelib
+          haxelib install hxcpp > /dev/null --quiet
+          haxelib install hxvlc > /dev/null --quiet
+          chmod +x ./setup/unix.sh
+          sh ./setup/unix.sh
 
-	#if VIDEOS_ALLOWED
-	private function playStartupVideo():Void
-	{
-		var splash = new VLC("assets/videos/splash.ogv");
-		splash.play();
-		addChild(splash);
+      - name: Create Version Tag
+        run: echo "${{ github.run_id }}" > VERSION
 
-		// When video finishes, start engine
-		splash.onEndReached = function() {
-			removeChild(splash);
-			startEngine();
-		}
+      - name: Compile
+        run: haxelib run lime build mac --app-version="4.0.0-${{ github.run_id }}" -D officialBuild -D VIDEOS_ALLOWED
 
-		// Optional: skip video with ENTER or SPACE
-		stage.addEventListener(openfl.events.Event.ENTER_FRAME, function(_) {
-			if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE) {
-				splash.stop();
-				removeChild(splash);
-				startEngine();
-			}
-		});
-	}
-	#end
-
-	private function startEngine():Void
-	{
-		#if LUA_ALLOWED
-		Mods.pushGlobalMods();
-		#end
-		Mods.loadTopMod();
-
-		FlxG.save.bind('funkin', CoolUtil.getSavePath());
-		Highscore.load();
-
-		#if HSCRIPT_ALLOWED
-		setupIrisHandlers();
-		#end
-
-		Controls.instance = new Controls();
-		ClientPrefs.loadDefaultKeys();
-
-		#if ACHIEVEMENTS_ALLOWED
-		Achievements.load();
-		#end
-
-		addChild(new FlxGame(game.width, game.height, game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
-
-		#if !mobile
-		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
-		addChild(fpsVar);
-		Lib.current.stage.align = "tl";
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		if(fpsVar != null)
-			fpsVar.visible = ClientPrefs.data.showFPS;
-		#end
-
-		#if (linux || mac)
-		var icon = Image.fromFile("icon.png");
-		Lib.current.stage.window.setIcon(icon);
-		#end
-
-		#if html5
-		FlxG.autoPause = false;
-		FlxG.mouse.visible = false;
-		#end
-
-		FlxG.fixedTimestep = false;
-		FlxG.game.focusLostFramerate = 60;
-		FlxG.keys.preventDefaultKeys = [TAB];
-
-		#if CRASH_HANDLER
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
-		#end
-
-		#if DISCORD_ALLOWED
-		DiscordClient.prepare();
-		#end
-
-		FlxG.signals.gameResized.add(function (w, h) {
-			if (FlxG.cameras != null)
-				for (cam in FlxG.cameras.list)
-					if (cam != null && cam.filters != null)
-						resetSpriteCache(cam.flashSprite);
-
-			if (FlxG.game != null)
-				resetSpriteCache(FlxG.game);
-		});
-	}
-
-	static function resetSpriteCache(sprite:Sprite):Void
-	{
-		@:privateAccess {
-			sprite.__cacheBitmap = null;
-			sprite.__cacheBitmapData = null;
-		}
-	}
-
-	#if HSCRIPT_ALLOWED
-	private function setupIrisHandlers():Void
-	{
-		Iris.warn = function(x, ?pos:haxe.PosInfos) { logIris("WARNING", x, pos); }
-		Iris.error = function(x, ?pos:haxe.PosInfos) { logIris("ERROR", x, pos); }
-		Iris.fatal = function(x, ?pos:haxe.PosInfos) { logIris("FATAL", x, pos); }
-	}
-
-	private function logIris(level:String, x:String, pos:haxe.PosInfos):Void
-	{
-		var newPos:HScriptInfos = cast pos;
-		if (newPos.showLine == null) newPos.showLine = true;
-		var msgInfo:String = (newPos.funcName != null ? '(${newPos.funcName}) - ' : '') + '${newPos.fileName}:';
-		#if LUA_ALLOWED
-		if (newPos.isLua == true) {
-			msgInfo += 'HScript:';
-			newPos.showLine = false;
-		}
-		#end
-		if (newPos.showLine == true) {
-			msgInfo += '${newPos.lineNumber}:';
-		}
-		msgInfo += ' $x';
-		if (PlayState.instance != null)
-			PlayState.instance.addTextToDebug(level + ': $msgInfo', level == "WARNING" ? FlxColor.YELLOW : 0xFFBB0000);
-	}
-	#end
-
-	#if CRASH_HANDLER
-	function onCrash(e:UncaughtErrorEvent):Void
-	{
-		var errMsg:String = "";
-		var path:String;
-		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
-		var dateNow:String = Date.now().toString();
-		dateNow = dateNow.replace(" ", "_");
-		dateNow = dateNow.replace(":", "'");
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
-
-		for (stackItem in callStack)
-		{
-			switch (stackItem)
-			{
-				case FilePos(s, file, line, column):
-					errMsg += file + " (line " + line + ")\n";
-				default:
-					Sys.println(stackItem);
-			}
-		}
-
-		errMsg += "\nUncaught Error: " + e.error;
-		#if officialBuild
-		errMsg += "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine";
-		#end
-		errMsg += "\n\n> Crash Handler written by: sqirra-rng";
-
-		if (!FileSystem.exists("./crash/"))
-			FileSystem.createDirectory("./crash/");
-
-		File.saveContent(path, errMsg + "\n");
-
-		Sys.println(errMsg);
-		Sys.println("Crash dump saved in " + Path.normalize(path));
-
-		Application.current.window.alert(errMsg, "Error!");
-		#if DISCORD_ALLOWED
-		DiscordClient.shutdown();
-		#end
-		Sys.exit(1);
-	}
-	#end
-}
+      - name: Publish Artifact
+        uses: actions/upload-artifact@v4.3.4
+        with:
+          name: macBuild
+          path: export/release/macos/bin
